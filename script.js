d6f4e369049971d761101b9f9cc96d79b607ad6f
“joaohenchs” <joao.delosrioshenchs@hotmail.com>
2023-08-23 19:25:23 -0300

    🎉 initial commit

--- script.js
+++ script.js
@@ -1,28 +1,16 @@
 function toggleMode() {
-  //pegando o Html ou body
   const html = document.documentElement
-  //enviando instrução para trocar cor
   html.classList.toggle("light")
 
-  //pegar a tag img
-  //substituir a imagem
-  //se tiver ligth mode,adicionar a imagem ligth
-  //set tiver sem ligth mode. manter a imagem normar
-}
+  // pegar a tag img
+  const img = document.querySelector("#profile img")
 
-//function toggleMode() {
-//  //pegando o Html ou body
-//  const html = document.documentElement
-//  //
-//  //if (html.classList.contains("light")) {
-//  //  html.classList.remove("light")
-//  //} else {
-//  //  html.classList.add("light")
-//
-//  html.classList.toggle("light")
-//
-//  //pegar a tag img
-//  //substituir a imagem
-//  //se tiver ligth mode,adicionar a imagem ligth
-//  //set tiver sem ligth mode. manter a imagem normar
-//}
+  // substituir a imagem
+  if (html.classList.contains("light")) {
+    // se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "./assets/avatar.png")
+  } else {
+    // set tiver sem light mode, manter a imagem normal
+    img.setAttribute("src", "./assets/avatar.png")
+  }
+}
